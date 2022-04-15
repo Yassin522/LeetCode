76a5019d9b4c9048244cd458c84c4335a6f6ff58
Yassin Abdulmahdi <88105077+Yassin522@users.noreply.github.com>
2022-04-15 22:29:56 +0300

    Time: 45 ms (65.32%), Space: 27.5 MB (92.44%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1;
+        while(l<=r){
+             int m=(l+r)/2;
+            if(nums[m]==target)
+                 return m;
+            
+            else if(target>nums[m])
+                  l++;
+            else
+                r--;
+            
+        }
+        return -1;
+    }
+};
